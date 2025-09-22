86a3611239e0c2c5043c49f2e78ca095f94ecdc2
Habiba Hmed <habibamohamed3880@gmail.com>
2025-09-22 19:08:55 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useTranslation } from "react-i18next";
-function App() {
-  const { t } = useTranslation();
+import { useTranslation } from "react-i18next"
 
+function App() {
+
+  const { t } = useTranslation();
+  
   return (
     <div>
       <h1>{t("welcome")}</h1>
-      <LanguageToggle />
     </div>
-  );
+  )
 }
 
 export default App;
