95e8ffd3da4e25e5a8f162df81a71c99f5b2a8b9
Habiba Hmed <habibamohamed3880@gmail.com>
2025-10-01 22:58:04 +0300

    change img position

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -26,140 +26,148 @@ export default function SignUp() {
   };
 
   return (
-    <div className={styles["signup-container"]}>
-      <div className={styles["signup-img"]}>
-        <img src={logo} alt="Logo" className={styles["signup-logo"]} />
+    <div className={styles["signup-page"]}>
+      <div className={styles["signup-left"]}>
+        <img src={logo} alt="Logo" />
       </div>
 
-      <h2>Create a new account</h2>
-      <p>It's quick and easy.</p>
-
-      <form onSubmit={handleSubmit} className={styles["signup-form"]}>
-        <div className={styles["row"]}>
-          <input
-            type="text"
-            name="firstName"
-            placeholder="First name"
-            value={form.firstName}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="text"
-            name="lastName"
-            placeholder="Surname"
-            value={form.lastName}
-            onChange={handleChange}
-            required
-          />
+      <div className={styles["signup-container"]}>
+        <div className={styles["signup-img"]}>
+          <img src={logo} alt="Logo" className={styles["signup-logo"]} />
         </div>
 
-        <label>Date of birth</label>
-        <div className={styles["row"]}>
-          <select
-            name="dobDay"
-            value={form.dobDay}
-            onChange={handleChange}
-            required
-          >
-            <option value="">Day</option>
-            {[...Array(31)].map((_, i) => (
-              <option key={i + 1}>{i + 1}</option>
-            ))}
-          </select>
+        <h2>Create a new account</h2>
+        <p>It's quick and easy.</p>
 
-          <select
-            name="dobMonth"
-            value={form.dobMonth}
-            onChange={handleChange}
-            required
-          >
-            <option value="">Month</option>
-            <option>Jan</option>
-            <option>Feb</option>
-            <option>Mar</option>
-            <option>Apr</option>
-            <option>May</option>
-            <option>Jun</option>
-            <option>Jul</option>
-            <option>Aug</option>
-            <option>Sep</option>
-            <option>Oct</option>
-            <option>Nov</option>
-            <option>Dec</option>
-          </select>
-
-          <select
-            name="dobYear"
-            value={form.dobYear}
-            onChange={handleChange}
-            required
-          >
-            <option value="">Year</option>
-            {Array.from({ length: 100 }, (_, i) => 2025 - i).map((year) => (
-              <option key={year}>{year}</option>
-            ))}
-          </select>
-        </div>
-
-        <label>Gender</label>
-        <div className={styles["row"] + " " + styles["gender"]}>
-          <label>
+        <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+          <div className={styles["row"]}>
             <input
-              type="radio"
-              name="gender"
-              value="Female"
+              type="text"
+              name="firstName"
+              placeholder="First name"
+              value={form.firstName}
               onChange={handleChange}
               required
-            />{" "}
-            Female
-          </label>
-          <label>
+            />
             <input
-              type="radio"
-              name="gender"
-              value="Male"
+              type="text"
+              name="lastName"
+              placeholder="Surname"
+              value={form.lastName}
               onChange={handleChange}
-            />{" "}
-            Male
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="gender"
-              value="Custom"
+              required
+            />
+          </div>
+
+          <label>Date of birth</label>
+          <div className={styles["row"]}>
+            <select
+              name="dobDay"
+              value={form.dobDay}
               onChange={handleChange}
-            />{" "}
-            Custom
-          </label>
-        </div>
+              required
+            >
+              <option value="">Day</option>
+              {[...Array(31)].map((_, i) => (
+                <option key={i + 1}>{i + 1}</option>
+              ))}
+            </select>
 
-        <input
-          type="text"
-          name="email"
-          placeholder="Mobile number or email address"
-          value={form.email}
-          onChange={handleChange}
-          required
-        />
+            <select
+              name="dobMonth"
+              value={form.dobMonth}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Month</option>
+              <option>Jan</option>
+              <option>Feb</option>
+              <option>Mar</option>
+              <option>Apr</option>
+              <option>May</option>
+              <option>Jun</option>
+              <option>Jul</option>
+              <option>Aug</option>
+              <option>Sep</option>
+              <option>Oct</option>
+              <option>Nov</option>
+              <option>Dec</option>
+            </select>
 
-        <input
-          type="password"
-          name="password"
-          placeholder="New password"
-          value={form.password}
-          onChange={handleChange}
-          required
-        />
+            <select
+              name="dobYear"
+              value={form.dobYear}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Year</option>
+              {Array.from({ length: 100 }, (_, i) => 2025 - i).map((year) => (
+                <option key={year}>{year}</option>
+              ))}
+            </select>
+          </div>
 
-        <br />
+          <label>Gender</label>
+          <div className={styles["row"] + " " + styles["gender"]}>
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="Female"
+                onChange={handleChange}
+                required
+              />{" "}
+              Female
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="Male"
+                onChange={handleChange}
+              />{" "}
+              Male
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="Custom"
+                onChange={handleChange}
+              />{" "}
+              Custom
+            </label>
+          </div>
 
-        <button type="submit" className={styles["signup-btn"]}>
-          Sign Up
-        </button>
-      </form>
+          <input
+            type="text"
+            name="email"
+            placeholder="Mobile number or email address"
+            value={form.email}
+            onChange={handleChange}
+            required
+          />
 
-      <p className={styles["already"]}>Already have an account? Go to login</p>
+          <input
+            type="password"
+            name="password"
+            placeholder="New password"
+            value={form.password}
+            onChange={handleChange}
+            required
+          />
+
+          <br />
+
+          <button type="submit" className={styles["signup-btn"]}>
+            Sign Up
+          </button>
+        </form>
+
+        <p className={styles["already"]}>
+          Already have an account? Go to login
+        </p>
+      </div>
     </div>
   );
 }
