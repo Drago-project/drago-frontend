379afe1b4ead53015b54f7c5a46fe7717a885fab
Habiba Hmed <habibamohamed3880@gmail.com>
2025-10-24 13:38:39 +0300

    sitting up home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import volcano from "../assets/islands/Adventure Games Island.svg";
+import pyramids from "../assets/islands/History Stories Island.svg";
+import writing from "../assets/islands/Writing Workshop Island.svg";
+import books from "../assets/islands/island1.svg";
+import animate1 from "../assets/animation/drago(holding map 1).svg";
+import animate2 from "../assets/animation/drago(holding map 2).svg";
+// import books from "../assets/islands/";
+
 function Home() {
     return (
         <div>
-            <p>Welcome to the Home Page</p>
+            <img src={volcano} alt="Volcano Island" />
+            <img src={pyramids} alt="Pyramids Island" />
+            <img src={writing} alt="Writing Workshop Island" />
+            <img src={books} alt="Books Island" />
+
+            <img src={animate1} alt="Drago holding map 1" />
+            <img src={animate2} alt="Drago holding map 2" />
+            <p>help drago find his way to the treasure!</p>
         </div>
     )
 }
