ccf2a53d8c8a21fcd1ceb5069fead80e04c89bab
Habiba Hmed <habibamohamed3880@gmail.com>
2026-06-13 16:36:35 +0300

    fix: correct import path for PreTestApp component

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import NavBar from "./components/NavBar";
 import NavInside from "./components/NavInside";
 import ProtectedRoute from "./components/ProtectedRoute";
 import ResetPassword from "./components/ResetPassword";
-import PreTestApp from "./PreTest/PreTestApp";
+import PreTestApp from "./preTest/PreTestApp";
 // Games
 import VolcanoWords from "./games/VolcanoWords";
 import ReadingQuest from "./games/ReadingQuest";
