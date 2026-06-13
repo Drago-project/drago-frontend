c0d3d695a6ac7d4caf2edf1b407533077ac84d2f
Habiba Hmed <habibamohamed3880@gmail.com>
2026-06-13 16:42:19 +0300

    change preTest file name ti fix

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import NavBar from "./components/NavBar";
 import NavInside from "./components/NavInside";
 import ProtectedRoute from "./components/ProtectedRoute";
 import ResetPassword from "./components/ResetPassword";
-import PreTestApp from "./preTest/PreTestApp";
+import PreTestApp from "./fix/PreTestApp";
 // Games
 import VolcanoWords from "./games/VolcanoWords";
 import ReadingQuest from "./games/ReadingQuest";
